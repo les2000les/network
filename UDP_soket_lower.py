3c75dbc46999182e7ea585d1c1d169c6eba34bde
les2000les <72508211+les2000les@users.noreply.github.com>
2022-07-06 13:50:39 +0900

    Create UDP_soket_lower.py

---
+++ UDP_soket_lower.py
@@ -0,0 +1,25 @@
+import socket #UDP Client
+if __name__ == '__main__':
+    serverName = 'localhost' #IP 주소
+    serverPort = 17000 #Port 주소
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+
+    while True:
+        print('Client >> ', end='')
+        send_data = bytes(input().encode()) #사용자 입력
+        clientSocket.sendto(send_data, (serverName, serverPort)) 
+        recv_data, addr = clientSocket.recvfrom(2048)
+        print('Server >> ' + str(recv_data.decode()))
+
+import socket #UDP Server
+
+if __name__ == '__main__':
+    serverName = '' #IP 주소
+    serverPort = 17000 #Port 주소
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serverSocket.bind((serverName, serverPort))
+
+    while True:
+        data, addr = serverSocket.recvfrom(2048) 
+        print('Client >> ' + str(data))
+        serverSocket.sendto(data.lower(), (addr)) 
